438184f59712bee85a28c905f5c33001566bfdf7
vincenzo ceraldi <ceraldicontabilita@gmail.com>
2026-04-08 04:23:02 +0200

    feat: esporta parse_presenze_pdf e is_presenze_pdf

--- app/parsers/__init__.py
+++ app/parsers/__init__.py
@@ -2,9 +2,9 @@
 
 from .fattura_xml import parse_fattura_xml
 from .cedolino_zucchetti import parse_cedolino_pdf
+from .presenze_zucchetti import parse_presenze_pdf, is_presenze_pdf
 from .estratto_conto_bpm import parse_estratto_conto_pdf
 from .f24 import parse_f24_pdf
 from .corrispettivi_xml import parse_corrispettivo_xml
 from .distinta_bpm import parse_distinta_pdf
 from .verbale import parse_verbale_pdf, parse_verbale_text
-
