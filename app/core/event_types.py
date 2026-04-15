52fd2a3fa87404e8e915cb65451049c8c9646c61
vincenzo ceraldi <ceraldicontabilita@gmail.com>
2026-04-15 19:12:11 +0200

    Add event types constants

---
+++ app/core/event_types.py
@@ -0,0 +1,6 @@
+# Event type constants
+DOCUMENT_RECEIVED = "document_received"
+VERBALE_RICEVUTO = "verbale_ricevuto"
+CEDOLINO_RICEVUTO = "cedolino_ricevuto"
+BANK_IMPORTED = "bank_imported"
+PAYMENT_DETECTED = "payment_detected"
