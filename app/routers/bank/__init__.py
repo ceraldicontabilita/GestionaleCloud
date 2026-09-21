71076ec2eed2ce29183beff1cb9bfd39b3e10ef9
vincenzo ceraldi <ceraldicontabilita@gmail.com>
2026-09-21 07:09:12 +0200

    Fase 1C: rimuove export package del router POS morto

--- app/routers/bank/__init__.py
+++ app/routers/bank/__init__.py
@@ -3,11 +3,9 @@ from . import bank_statement_import
 from . import estratto_conto
 # NOTA: archivio_bonifici modularizzato in /app/app/routers/bonifici_module/
 from . import assegni
-from . import pos_accredito
 
 __all__ = [
     'bank_statement_import',
     'estratto_conto',
-    'assegni',
-    'pos_accredito'
+    'assegni'
 ]
